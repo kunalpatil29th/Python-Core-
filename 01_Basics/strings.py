0fba4d653928b9b7686e4839f29b708f9b1aacea
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-03 19:04:30 +0530

    feat: add strings fundamental with definition

---
+++ 01_Basics/strings.py
@@ -0,0 +1,20 @@
+# strings.py
+
+"""
+Definition: A string is a sequence of characters enclosed in single quotes (''), 
+double quotes (""), or triple quotes (''' or """). Strings are immutable.
+"""
+
+# Creating strings
+first_name = "Kunal"
+last_name = 'Patil'
+
+# Concatenation
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+# String length
+print("Length:", len(full_name))
+
+# Accessing characters (indexing)
+print("First character:", full_name[0])
