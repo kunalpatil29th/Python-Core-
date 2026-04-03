7edaab8362167af473dae8f79077be103aa9bba6
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-03 19:05:23 +0530

    feat: add docstring example to comments

--- 01_Basics/comments.py
+++ 01_Basics/comments.py
@@ -13,3 +13,12 @@ This is used for explanation
 """ 
 
 print("Hello World") 
+
+def add(a, b):
+    """
+    This is a docstring.
+    It explains that this function adds two numbers.
+    """
+    return a + b
+
+print("Sum:", add(5, 10))
