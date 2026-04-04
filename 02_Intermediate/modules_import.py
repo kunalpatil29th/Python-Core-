cec40f78fef0319944432ac37218a0b6db24114b
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-04 20:03:48 +0530

    feat: add modules and import intermediate module with definition

---
+++ 02_Intermediate/modules_import.py
@@ -0,0 +1,17 @@
+# modules_import.py
+
+"""
+Definition: A module is a file containing Python definitions and statements. 
+Modules help in organizing code into separate files for better maintainability and reusability.
+"""
+
+import math
+import datetime
+
+# Using functions from the math module
+print("Square root of 16:", math.sqrt(16))
+print("Value of Pi:", math.pi)
+
+# Using functions from the datetime module
+current_time = datetime.datetime.now()
+print("Current Date and Time:", current_time)
