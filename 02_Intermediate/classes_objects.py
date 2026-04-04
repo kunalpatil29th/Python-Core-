5a41cdba22c47e2b13e15eedcdbeec8b0a637cfa
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-04 20:03:45 +0530

    feat: add classes and objects intermediate module with definition

---
+++ 02_Intermediate/classes_objects.py
@@ -0,0 +1,23 @@
+# classes_objects.py
+
+"""
+Definition: Object-Oriented Programming (OOP) is a paradigm based on the concept of "objects," 
+which can contain data (attributes) and code (methods). A 'class' is a blueprint for objects.
+"""
+
+# Defining a class
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+# Creating objects (instances of a class)
+s1 = Student("Kunal", 20)
+s2 = Student("Rahul", 22)
+
+# Accessing attributes and methods
+s1.display_info()
+s2.display_info()
