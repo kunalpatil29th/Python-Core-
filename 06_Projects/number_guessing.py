0c5a5cb62b1be86e2d43c0a9afcc3f36ac884576
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-08 20:45:38 +0530

    feat: add number guessing game project contribution

---
+++ 06_Projects/number_guessing.py
@@ -0,0 +1,25 @@
+# number_guessing.py
+
+"""
+Project: Number Guessing Game
+The computer picks a random number, and the user tries to guess it with hints.
+"""
+
+import random
+
+def start_game():
+    secret_number = random.randint(1, 10)
+    print("I'm thinking of a number between 1 and 10.")
+    
+    # Simulated guess
+    guess = 5
+    print(f"User guesses: {guess}")
+    
+    if guess == secret_number:
+        print("Correct!")
+    elif guess < secret_number:
+        print("Too low!")
+    else:
+        print("Too high!")
+
+start_game()
