da11e9b7e78878de77e85b5faeea874a19ba0c7c
Kunal Patil <kunalpatil29th@gmail.com>
2026-04-18 00:11:06 +0530

    feat: add Flask basics backend module

---
+++ 21_Backend_Web_Development/flask_basics.py
@@ -0,0 +1,14 @@
+# flask_basics.py
+
+"""
+Definition: Flask is a micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries.
+"""
+
+def simulate_flask():
+    print("Starting Flask Development Server...")
+    print(" * Serving Flask app 'app'")
+    print(" * Debug mode: on")
+    print(" * Running on http://127.0.0.1:5000")
+    print("127.0.0.1 - - [18/Apr/2026 12:00:00] 'GET / HTTP/1.1' 200 -")
+
+simulate_flask()
