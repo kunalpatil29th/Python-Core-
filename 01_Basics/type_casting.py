a1b0e8ded95cd6062cd99390950d5ad86ac3c36d
Kunal Patil <kunalpatil29th@gmail.com>
2026-06-25 23:43:24 +0530

    Remove type casting definition comment
    Removed the definition comment for type casting.

--- 01_Basics/type_casting.py
+++ 01_Basics/type_casting.py
@@ -1,9 +1,5 @@
 # type_casting.py 
 
-"""
-Definition: Type casting (or type conversion) is the process of converting a variable from one data type to another 
-(e.g., converting a string "10" to an integer 10).
-"""
 
 a = "10" 
 b = 5 
